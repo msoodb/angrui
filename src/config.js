1c5e0e1acaa7a72dc5d8fd5456768120fc8d4fbc
masoud bolhassani <masoud.bolhassani@gmail.com>
2019-06-08 12:20:18 +0430

    new item

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export function baseurl(){
-  //return 'http://127.0.0.1:9081';
-  return 'http://185.83.115.151:8592';
+  return 'http://127.0.0.1:9081';
+  //return 'http://185.83.115.151:8592';
   //return 'http://198.143.183.251';
   //return 'https://api.angru.cloudns.asia';
 }
--- src/views/aggregators/Aggregator.vue
+++ src/views/aggregators/Aggregator.vue
@@ -78,19 +78,19 @@ export default {
       required: true
     }
   },
-  watch: {
-    record_id: {
-      immediate: true,
-      handler(newVal, oldVal) {
-        this.record_id = newVal;
-        if(this.record_id == '-1'){
-          this.getDefaultData();
-        } else{
-          this.getItem();
-        }
-      }
-    }
-  },
+  // watch: {
+  //   record_id: {
+  //     immediate: true,
+  //     handler(newVal, oldVal) {
+  //       this.record_id = newVal;
+  //       if(this.record_id == '-1'){
+  //         this.getDefaultData();
+  //       } else{
+  //         this.getItem();
+  //       }
+  //     }
+  //   }
+  // },
   data: () => {
     return {
       form: {
@@ -137,11 +137,11 @@ export default {
   components: {
     'au-keyValue':AUKeyValue
   },
-  mounted(){
-    if(this.record_id != "-1"){
-      this.getItem();
-    }
-  },
+  // mounted(){
+  //   if(this.record_id != "-1"){
+  //     this.getItem();
+  //   }
+  // },
   methods: {
     onChangeDetails(val){
       this.form.details = val;
@@ -150,26 +150,27 @@ export default {
       this.status = selected;
       this.form.status = this.status['value'];
     },
-    getDefaultData(){
-      var self = this;
-      self.form.id = self.record_id;
-      self.form.name = '';
-      self.form.title = '';
-      self.form.code = '';
-      self.form.phone = '';
-      self.form.email = '';
-      self.form.details = '';
-      self.form.status = '1',
-      self.status = null;
-      self.form.type = '0';
-      self.form.situation = '0';
-      self.form.created_at = '';
-      self.form.updated_at = '';
-      self.form.description = '';
-    },
     getItem(){
       var self = this;
       var id = self.record_id;
+      if(id == '-1'){
+        self.form.id = self.record_id;
+        self.form.name = '';
+        self.form.title = '';
+        self.form.code = '';
+        self.form.phone = '';
+        self.form.email = '';
+        self.form.details = '';
+        self.form.status = '1',
+        self.status = null;
+        self.form.type = '0';
+        self.form.situation = '0';
+        self.form.created_at = '';
+        self.form.updated_at = '';
+        self.form.description = '';
+        self.$refs["form"].resetFields();
+        return;
+      }
       var token = JSON.parse(localStorage.getItem("jwtoken"));
       let config = {
         headers: {
