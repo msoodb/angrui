e799c494e140f56cef80752ece10ef5e252adf0b
masoud bolhassani <masoud.bolhassani@gmail.com>
2019-06-22 12:53:58 +0430

    https config

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export function baseurl(){
   //return 'http://127.0.0.1:9081';
   //return 'http://185.83.115.151:8592';
-  return 'http://23.95.132.59:8592';
+  return 'https://api.zeus.cloudns.org'//return 'http://23.95.132.59:8592';
   //return 'https://api.angru.cloudns.asia';
 }
