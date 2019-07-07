d7ad93045699bcebe973222125935f2a7548ec00
masoud bolhassani <masoud.bolhassani@gmail.com>
2019-07-07 16:19:44 +0430

    config

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 export function baseurl(){
   //return 'http://127.0.0.1:9081';
   //return 'http://185.83.115.151:8592';
-  return 'http://127.0.0.1';
+  //return 'http://127.0.0.1';
   //return 'https://api.zeus.cloudns.org'; //return 'http://23.95.132.59:8592';
   //return 'https://api.angru.cloudns.asia';
-  //return 'https://api.mibinim.com';
+  return 'https://api.mibinim.com';
 }
